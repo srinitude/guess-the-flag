7f09375abd8b90b4c18385676c5852d64b1611a0
Kiren Srinivasan <srinitude@gmail.com>
2020-10-06 13:49:14 -0400

    Initial Commit

---
+++ Guess the Flag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Guess the Flag
+//
+//  Created by Kiren Srinivasan on 10/6/20.
+//  Copyright © 2020 Kiren Srinivasan's Practice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
